8e633d8d7be4e9e195c32d1ccac8f0a62aecd7d0
garma <61622461+garma83@users.noreply.github.com>
2024-09-18 23:33:08 +0200

    a

--- tafel_test/multi_test.py
+++ tafel_test/multi_test.py
@@ -2,14 +2,17 @@ import random
 import os
 import argparse
 from colorama import Fore, Style
+import time
 
 def main(tafels):
 
     os.system('clear')
 
-    nrquestions = 20
-
+    nrquestions = 40
     score = 0
+
+    start_time = time.time()
+
     for i in range(nrquestions):
         tafel = random.choice(tafels)
         a = random.randint(1, 10)
@@ -20,10 +23,14 @@ def main(tafels):
             score += 1
         else:
             print(f"{Fore.RED}Helaas... Het goede antwoord is {a * tafel}.{Style.RESET_ALL}. Je had ingetypt: '{answer}'")
-    print(f"Van de {nrquestions} vragen heb je er {score} goed.")
+
+    end_time = time.time()
+    duration = end_time - start_time
+
+    print(f"Van de {nrquestions} vragen heb je er {score} goed in {duration:.2f} seconden.")
     with open("scores.txt", "a") as file:
         tafels_str = ", ".join([str(tafel) for tafel in tafels])
-        file.write(f"Score voor tafels {tafels_str}: {score} van {nrquestions}\n")
+        file.write(f"Score voor tafels {tafels_str}: {score} van {nrquestions} ({duration:.2f} seconden)\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
