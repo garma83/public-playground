339ea61ef871deb9dca03950cb5dac666a55590a
garma <61622461+garma83@users.noreply.github.com>
2025-03-24 10:20:22 +0100

    tres basic material

---
+++ tresjs_basic_material/my-tresjs-app/src/App.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+</script>
+
+<template>
+  <TresCanvas>
+    <TresPerspectiveCamera /><!-- Your scene goes here -->
+    <OrbitControls />
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="'#ffffff'" />
+    </TresMesh>
+  </TresCanvas>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
